05fe4ac01a3b47981a07cab96c0de845a89b94ee
Thomas Garlot <thomas@garlot.net>
2020-03-08 15:02:31 +0100

    Remove useless DockerFile

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hugo server -b="http://www.docker-patterns.org" -s /data/site -d /data/site/public -p 80 --bind=0.0.0.0 --watch --appendPort=false --disableLiveReload &
-
-cd /data
-
-while true; do git pull origin master && sleep 60 ; done
